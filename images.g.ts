89b0f9f50ea90ad3f9de4acf5432de88b1a4d3dd
tzewei.algo <ong.tzewei@gmail.com>
2024-05-21 16:08:10 +0800

    Update pxt.json, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,82 +3,7 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image1":
-            case "myRobotDown":return img`
-. . . . . . f f f f . . . . . . 
-. . . . . . f . . f . . . . . . 
-. . . . . . f f f f . . . . . . 
-. . . . . . . f f . . . . . . . 
-. . . f f f f f f f f f f . . . 
-. . . f d d d d d d d d f . . . 
-. . f f d d d d d d d d f f . . 
-. . f f d d f d d f d d f f . . 
-. . f f d d f d d f d d f f . . 
-. . . f d d d d d d d d f . . . 
-. . . f f f f f f f f f f . . . 
-. . b d f 1 1 1 1 1 1 f d b . . 
-. . d . f 1 1 1 1 1 1 f . d . . 
-. . d d f 1 1 1 1 1 1 f d d . . 
-. . . . . f f f f f f . . . . . 
-. . . . . f f . . f f . . . . . 
-`;
-            case "image2":
-            case "myRobotLeft":return img`
-. . . . . . . f f f . . . . . . 
-. . . . . . . f . f . . . . . . 
-. . . . . . . f f f . . . . . . 
-. . . . . . . . f . . . . . . . 
-. . . f f f f f f f f f . . . . 
-. . . f d d d d d d d f . . . . 
-. . . f d d d d d d d f . . . . 
-. . . f d d d f d d d f . . . . 
-. . . f d d d f d d d f . . . . 
-. . . f f d d d d d d f . . . . 
-. . . . f f f f f f f f . . . . 
-. . . . f 1 1 d d 1 1 f . . . . 
-. . . . f 1 1 b d 1 1 f . . . . 
-. . . . f 1 1 d d 1 1 f . . . . 
-. . . . . f f f f f f . . . . . 
-. . . . . . . f f f . . . . . . 
-`;
-            case "image3":
-            case "myRobotRight":return img`
-. . . . . . f f f . . . . . . . 
-. . . . . . f . f . . . . . . . 
-. . . . . . f f f . . . . . . . 
-. . . . . . . f . . . . . . . . 
-. . . . f f f f f f f f f . . . 
-. . . . f d d d d d d d f . . . 
-. . . . f d d d d d d d f . . . 
-. . . . f d d d f d d d f . . . 
-. . . . f d d d f d d d f . . . 
-. . . . f d d d d d d f f . . . 
-. . . . f f f f f f f f . . . . 
-. . . . f 1 1 d d 1 1 f . . . . 
-. . . . f 1 1 d b 1 1 f . . . . 
-. . . . f 1 1 d d 1 1 f . . . . 
-. . . . . f f f f f f . . . . . 
-. . . . . . f f f . . . . . . . 
-`;
-            case "image4":
-            case "myRobotUp":return img`
-. . . . . . f f f f . . . . . . 
-. . . . . . f . . f . . . . . . 
-. . . . . . f f f f . . . . . . 
-. . . . . . . f f . . . . . . . 
-. . . f f f f f f f f f f . . . 
-. . . f f f f f f f f f f . . . 
-. . f f f f f f f f f f f f . . 
-. . f f f f f f f f f f f f . . 
-. . f f d d d d d d d d f f . . 
-. . . f d d d d d d d d f . . . 
-. . . f f f f f f f f f f . . . 
-. . b d f 1 1 1 1 1 1 f d b . . 
-. . d . f 1 1 1 1 1 1 f . d . . 
-. . d d f 1 1 1 1 1 1 f d d . . 
-. . . . . f f f f f f . . . . . 
-. . . . . f f . . f f . . . . . 
-`;
+
         }
         return null;
     })
@@ -96,12 +21,6 @@ namespace myImages {
         }
         return null;
     })
-
-}
-// Auto-generated code. Do not edit.
-
-// Auto-generated code. Do not edit.
-namespace myTiles {
 
 }
 // Auto-generated code. Do not edit.
